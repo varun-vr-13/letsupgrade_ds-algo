55e692b488fc99f4e0a3bf64015cd192bd96d2c2
Varun <67255279+varun-vr-13@users.noreply.github.com>
2020-12-30 20:57:07 +0530

    Add files via upload

---
+++ insertion.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j, temp;
+    int arr[64];
+ 
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
+ 
+    printf("Enter %d integers\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 1 ; i <= n - 1; i++)
+    {
+	    j = i;
+            while ( j > 0 && arr[j-1] > arr[j])
+            {	        
+                temp     = arr[j];
+                arr[j]   = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+    }
+    printf("Sorted list in ascending order:\n");
+    for (i = 0; i <= n - 1; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
+/* 
+OUTPUT
+-------------
+Enter number of elements
+8
+Enter 8 integers
+19 89 67 56 09 90 27 71
+Sorted list in ascending order:
+9
+19
+27
+56
+67
+71
+89
+90
+
+
+
+*/
